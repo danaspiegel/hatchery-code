e5d34e2f2e66a6ed8346f176a6c95c5a7cc6fc3a
Dana Spiegel <dana@sociabledesign.com>
2012-11-12 02:07:35 +0000

    fixed incorrect view direction

--- urls.py
+++ urls.py
@@ -4,9 +4,9 @@ from django.views.generic.simple import direct_to_template
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('project.views',
-    url(r'^$', direct_to_template, { 'template': 'index.html' }, name='home'),
-    url(r'^index2/', 'index2', name='index2'),
+urlpatterns = patterns('',
+    url(r'^$', direct_to_template, { 'template': 'index5.html' }, name='home'),
+    url(r'^index2/', 'project.views.index2', name='index2'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^softball/', include('softball.urls')),
 )
