30df9a1aa62ddea7d9c2832ad179164a1fad4cbc
Dana Spiegel <dana@sociabledesign.com>
2012-11-13 05:23:50 +0000

    update from class

--- softball/urls.py
+++ softball/urls.py
@@ -1,9 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic.simple import direct_to_template
 
-from django.contrib import admin
-admin.autodiscover()
-
-urlpatterns = patterns('softball.views',
-    url(r'^$', 'team_list', name='team_list'),
+urlpatterns = patterns('',
+    url(r'^$', 'softball.views.team_list', name='team_list'),
 )
--- softball/views.py
+++ softball/views.py
@@ -6,7 +6,8 @@ def team_list(request):
     """
     Lists all teams in the Database
     """
+    teams = models.Team.objects.all().order_by('name')
     return TemplateResponse(request, 'softball/teams/list.html', {
-        'teams': models.Team.objects.all(),
+        'teams': teams,
     })
 
